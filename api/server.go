4805c747f037f4966a1103bbb90b75da66fbb224
DaVonte Joseph Carter Vault <davonte.cartervault@outlook.com>
2025-01-06 10:16:03 -0500

    Sanitized

--- api/server.go
+++ api/server.go
@@ -3,14 +3,9 @@ package api
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/victorsteven/fullstack/api/controllers"
-	"github.com/victorsteven/fullstack/api/seed"
 )
-
-var server = controllers.Server{}
 
 func init() {
 	// loads values from .env into the system
@@ -21,18 +16,11 @@ func init() {
 
 func Run() {
 
-	var err error
-	err = godotenv.Load()
+	var err = godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error getting env, %v", err)
 	} else {
 		fmt.Println("We are getting the env values")
 	}
-
-	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-
-	seed.Load(server.DB)
-
-	server.Run(":8080")
 
 }
